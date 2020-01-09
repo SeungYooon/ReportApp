62846c7898fc3b0883890619ac2275fc9340b183
SeungYooon <elvmaksrkwmdk@gmail.com>
2020-01-09 16:41:45 +0900

    gridview header

---
+++ app/src/main/java/com/example/kotlin/HeaderHolder.kt
@@ -0,0 +1,8 @@
+package com.example.kotlin
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class HeaderHolder(itemView: View) :RecyclerView.ViewHolder(itemView) {
+
+}
